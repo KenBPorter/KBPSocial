90d501fd9f922b7ed22a0f9f8490d5c8072b0144
Ken <porterk@KensMBP.local>
2016-12-09 15:22:17 -0500

    Initial social feed UI (L150)

--- KBPSocial/CustomCircleView.swift
+++ KBPSocial/CustomCircleView.swift
@@ -1,15 +1,15 @@
 //
-//  CustomUIView.swift
+//  CustomCircleView.swift
 //  KBPSocial
 //
-//  Created by Ken Porter on 2016-12-08.
+//  Created by Ken Porter on 2016-12-09.
 //  Copyright © 2016 Ken Porter. All rights reserved.
 //
 
 import UIKit
 
-class CustomUIView: UIView {
-
+class CustomCircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -18,9 +18,10 @@ class CustomUIView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        
-        layer.cornerRadius = 2.0
-        
     }
-
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+    }
 }
