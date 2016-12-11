e0f9aa5df1152b32e44d81d94d9f26cd9b4cdc1a
Ken <KenBPorter@gmail.com>
2016-12-11 16:50:33 -0500

    some post data to table view (L155)

--- KBPSocial/PostCell.swift
+++ KBPSocial/PostCell.swift
@@ -13,13 +13,26 @@ class PostCell: UITableViewCell {
     // @IBOutlets
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var usernameLbl: UILabel!
+    
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func configureCell(post: Post) {
+        
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+        
+        
+    }
+    
+    
 }
